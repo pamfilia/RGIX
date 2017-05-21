ab172fb5e821fde7df810b620b3a34e1897e912e
Oktay Kahraman <oktay.kahraman@sgs.com>
2017-05-21 18:51:31 +0300

    sync

---
+++ dist/script/main.9e6e87d4743d05882a03.js
@@ -0,0 +1,8 @@
+webpackJsonp([1],[
+/* 0 */
+/***/ (function(module, exports) {
+
+throw new Error("Module parse failed: C:\\other projects\\front-end\\node_modules\\awesome-typescript-loader\\dist\\entry.js!C:\\other projects\\front-end\\src\\script\\pages\\index.tsx Unexpected token (21:16)\nYou may need an appropriate loader to handle this file type.\n|     }\r\n|     App.prototype.render = function () {\r\n|         return (<header_1.Header />);\r\n|     };\r\n|     return App;\r");
+
+/***/ })
+],[0]);
