c6bf190c6a01b94cc41f444c06a2ccb688aa029b
Mehmet Gani Tombalak <mgani.tombalak@yahoo.com>
2018-02-25 02:15:43 +0300

    Backend project updated.NPM packages updated.:heart_eyes::cake:

---
+++ back-end/ApiTests/OSGB.Api.Tests/ValuesControllerTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace OSGB.Api.Tests
+{
+    public class ValuesControllerTests
+    {
+        [Fact]
+        public void ShouldReturnStringArray()
+        {
+            var controller = new OSGB.Api.Controllers.ValuesController();
+
+            //Given
+            IEnumerable<string> expected = new string[] { "value1", "value2" };
+            //When
+            IEnumerable<string> actual = controller.Get();
+            //Then
+            Assert.Equal(expected, actual);
+        }
+    }
+}
