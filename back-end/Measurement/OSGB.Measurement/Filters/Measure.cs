ee035e9923bcaba781399f0babb8bd7a2cfdf165
Mehmet Gani Tombalak <mgani.tombalak@yahoo.com>
2018-03-12 23:39:19 +0300

    Failing test project repaired.

--- back-end/Measurement/OSGB.Measurement/Filters/Measure.cs
+++ back-end/Measurement/OSGB.Measurement/Filters/Measure.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.ComponentModel.DataAnnotations;
-using System.Runtime.InteropServices.ComTypes;
 using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace OSGB.Measurement.Filters
