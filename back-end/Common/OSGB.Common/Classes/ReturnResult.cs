1e7d0a42fe3d4832d382a6bf657c125878fce710
Mehmet Gani Tombalak <mgani.tombalak@yahoo.com>
2018-03-13 23:32:41 +0300

    Test commit

--- back-end/Common/OSGB.Common/Classes/ReturnResult.cs
+++ back-end/Common/OSGB.Common/Classes/ReturnResult.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 using OSGB.Common.Enums;
-using OSGB.Common.Extensions.Object;
 using OSGB.Common.Interfaces;
 
 namespace OSGB.Common.Classes
