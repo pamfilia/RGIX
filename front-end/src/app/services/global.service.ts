99a29e34024113403281f952e976e3b0c08899e8
Mehmet Gani TOMBALAK <mgani.tombalak@yahoo.com>
2018-04-03 00:43:47 +0300

    Project file hotfix.Project clean up.

--- front-end/src/app/services/global.service.ts
+++ front-end/src/app/services/global.service.ts
@@ -3,10 +3,9 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class GlobalService {
 
-  ApiBaseUrl = 'http://localhost:5000/api/';
+  ApiBaseUrl = 'https://osgb.azurewebsites.net/api/';
   AjaxReqRetryCount = 3;
   constructor() {
     Object.freeze(this);
    }
-
 }
