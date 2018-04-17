cea1ba487751aae1679bd3059db47d5cfbfe0b27
Oktay Kahraman <oktaykahraman82@gmail.com>
2018-04-18 02:01:15 +0300

    interview update

--- front-end/src/app/modules/interview/interview.module.ts
+++ front-end/src/app/modules/interview/interview.module.ts
@@ -4,12 +4,17 @@ import { CommonModule } from '@angular/common';
 import { InterviewRoutingModule } from './interview-routing.module';
 import { InterviewComponent } from './components/interview/interview.component';
 import { InterviewDetailsComponent } from './components/interview-details/interview-details.component';
+import { InterviewService } from './services/interview-service.service';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
     InterviewRoutingModule
   ],
-  declarations: [InterviewComponent, InterviewDetailsComponent]
+  declarations: [InterviewComponent, InterviewDetailsComponent],
+  providers: [InterviewService],
+  entryComponents: [InterviewDetailsComponent]
+
 })
 export class InterviewModule { }
