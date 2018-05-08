c936742318af5436e2e4753fe835832876c19fb8
Oktay Kahraman <oktaykahraman82@gmail.com>
2018-05-08 22:55:59 +0300

    api and interview model update

--- back-end/Data/OSGB.Data/Entity/Proposal.cs
+++ back-end/Data/OSGB.Data/Entity/Proposal.cs
@@ -7,7 +7,6 @@ namespace OSGB.Data.Entity
     public class Proposal:IEntity
     {
         public string Id { get; set; }
-        public string InterviewId { get; set; }
         public string Proposer { get; set; }
         public DateTime ProposalDate { get; set; }
         public List<ServiceType> ServiceTypes { get; set; }
--- front-end/src/app/models/proposal/IProposal.ts
+++ front-end/src/app/models/proposal/IProposal.ts
@@ -2,7 +2,6 @@ import { IServiceType } from '../service-type/IServiceType';
 
 export interface IProposalModel {
     id: string;
-    interviewId: string;
     proposer: string;
     proposalDate: string;
     serviceTypes: Array<IServiceType>;
